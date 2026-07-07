a2b8ff49f704048eef31510ce39d72e43b35dfe4
ThuanLamTu <ktq4t123@gmail.com>
2026-07-07 11:01:43 +0700

    change footer

--- js/components/footer.js
+++ js/components/footer.js
@@ -2,7 +2,7 @@ export function renderFooter() {
   return `
     <footer class="main-footer">
       <div class="container footer-container">
-        <p>&copy; 2025 Thuận Lâm Tự. All rights reserved.</p>
+        <p> © 2025 Thuận Lâm Tự. Thanks for visiting my portfolio.</p>
         <div class="footer-links">
           <a href="https://github.com/ThuanLamTu" target="_blank">GitHub</a>
           <a href="http://facebook.com/letmedownslowly25/" target="_blank">Facebook</a>
